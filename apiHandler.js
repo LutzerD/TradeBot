09c3fd52ff0c5056fe0e689a3528bd217ca4c02c
David Lutzer <LutzerD@users.noreply.github.com>
2021-02-28 15:29:03 -0800

    Supports historical data queries

--- apiHandler.js
+++ apiHandler.js
@@ -3,19 +3,29 @@ const { exchange } = require("./binance.js");
 exports.historicalData = historicalData;
 
 function historicalData(req, res, next, q) {
-  console.log("Got:", req);
   const symbol = req.query.symbol || "BTCUSDT";
   const historical = exchange.historical(symbol);
+
   historical.then(function (response) {
     response = response.data;
     const timeline = response.map((datum) => new Date(datum.time));
+
     const data = [
       {
         y: response.map((datum) => datum.price),
         x: timeline,
       },
     ];
-    res.json(data); //todo: support metadata, like e.g. send symbol, exchange/source, etc.
+
+    const result = {
+      data: data,
+      stock: {
+        ticker: symbol,
+        exchange: "Binance",
+      },
+    };
+
+    res.json(result); //todo: support metadata, like e.g. send symbol, exchange/source, etc.
   });
 
   historical.catch(function (error) {
